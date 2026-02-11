0aeaff051f9bacf5bfdc9aa19e643b0a8d2a100f
mrchi1-gif <mrchicham11@gmail.com>
2026-02-11 05:17:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import telebot
 
-# التوكن ديالك اللي عطيتيني
+# التوكن ديالك اللي خديتي من BotFather
 TOKEN = '8038038213:AAF_41SBVowk0KaSAqrVQaV9H9yMAf5sldU'
 bot = telebot.TeleBot(TOKEN)
 
-# الـ ID ديالك باش يصيفط ليك أنتِ بوحدك
+# الـ ID ديالك اللي جبناه من idbot
 MY_CHAT_ID = '6683842631'
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
+@bot.message_handler(commands=['start'])
+def welcome(message):
     bot.send_message(MY_CHAT_ID, "✅ أهلاً Mita! البوت System Watcher دابا خدام ومربوط بحسابك 24/24 ساعة على سيرفر Koyeb.")
 
 @bot.message_handler(func=lambda message: True)
