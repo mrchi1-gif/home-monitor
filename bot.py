745259c4194fd8a2c87cfd931a4a0c86e06f22c7
mrchi1-gif <mrchicham11@gmail.com>
2026-02-11 05:08:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
-print("سيرفر المراقبة شغال...")
+import telebot
+
+# التوكن ديالك اللي عطيتيني
+TOKEN = '8038038213:AAF_41SBVowk0KaSAqrVQaV9H9yMAf5sldU'
+bot = telebot.TeleBot(TOKEN)
+
+# الـ ID ديالك باش يصيفط ليك أنتِ بوحدك
+MY_CHAT_ID = '6683842631'
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(MY_CHAT_ID, "✅ أهلاً Mita! البوت System Watcher دابا خدام ومربوط بحسابك 24/24 ساعة على سيرفر Koyeb.")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "راني خدام وكنسمعك مزيان!")
+
+print("البوت بدأ العمل بنجاح...")
+bot.polling()
